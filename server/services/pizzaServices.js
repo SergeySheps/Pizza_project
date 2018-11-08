e81672cdd561f4003901ec9dab19bc66a1cac62e
Serje <shepelevich.serjew@gmail.com>
2018-11-08 12:01:05 +0300

    database moved to mLab

--- server/services/pizzaServices.js
+++ server/services/pizzaServices.js
@@ -2,9 +2,12 @@ const db = require('../helpers/dbHelpers')
 const {perPageLimit} = require('../constants/constants')
 const Ingredients = db.Ingredients
 const Pizza = db.Pizza
+const mail = require("../helpers/mailHelper");
 
 async function getProducts(req) {
   const queryStringValues = req.query
+
+  // mail();
 
   return queryStringValues.numPage
     ? await Pizza.paginate(
